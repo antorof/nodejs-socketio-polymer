e320dd73231a01aaaca1b8595bdf71ee64d2d84c
antorof <antorof.dev@gmail.com>
2015-04-25 18:31:47 +0200

    mend

--- sockets.js
+++ sockets.js
@@ -2,7 +2,7 @@ module.exports = function(io) {
   console.log("-- SOCKET.IO STARTED");
 
   io.sockets.on('connection', function (socket) {
-    console.log("NUEVA CONEXION")
+    console.log("NUEVA CONEXION");
     socket.on('captain', function(data) {
       console.log(data);
       socket.emit('Hello');
