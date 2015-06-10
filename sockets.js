4b3d7399f23b4d9f5df60a25d6b496943408310b
antorof <antorof.dev@gmail.com>
2015-06-10 18:59:00 +0200

    Some styles removed , updated index with more components' tests'.

--- sockets.js
+++ sockets.js
@@ -2,7 +2,7 @@ module.exports = function(io) {
   console.log("-- SOCKET.IO STARTED");
 
   io.sockets.on('connection', function (socket) {
-    console.log("NUEVA CONEXION");
+    console.log("[io] NUEVA CONEXION");
     socket.emit("news","Hello visitor");
     socket.on('captain', function(data) {
       console.log(data);
