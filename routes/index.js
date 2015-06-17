6f1959e7d3a5d3f0e47892c7f3f2160a6e780148
antorof <antorof.dev@gmail.com>
2015-06-17 19:59:14 +0200

    Backup of index in index-prev, index cleanup, socket.io local instead of cdn.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Components' });
+});
+router.get('/prev', function(req, res, next) {
+  res.render('index-prev', { title: 'Index - PREV' });
 });
 
 router.get('/:nombre', function(req, res, next) {
